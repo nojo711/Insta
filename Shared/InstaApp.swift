cb3c866fb55b8bd9a1b0c6258bc07dee8c889be7
Noah Tyree <noahjtyree@gmail.com>
2021-11-12 20:01:24 -0500

    Initial Commit

---
+++ Shared/InstaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaApp.swift
+//  Shared
+//
+//  Created by Noah Tyree on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct InstaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
